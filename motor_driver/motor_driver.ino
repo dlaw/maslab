dae692c64e30c33d352bc9c91467f4e4faa814f1
muffinator <muffin@mit.edu>
2012-01-10 15:16:44 -0500

    fixed baud rate for UART2 (motor driver comm)

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -3,21 +3,17 @@ void drive(char motor, char vel);
 void usart2_init(unsigned int baud);
 void usart2_tx(unsigned char data);
 
-unsigned int baud = 3 //250k baud rate
-unsigned int baud2 = 12 //38.4k baud rate
+unsigned int baud = 3; //250k baud rate
+unsigned int baud2 = 25; //38.4k baud rate
 
 void setup(){
   int serial;
-  Serial.begin(384000);
+  //Serial.begin(384000);
   usart2_init(baud2);
   usart2_tx(0xaa);
 }
 
 void loop(){
-  if (Serial.available() > 0) {
-    serial = Serial.read();
-  
-  }
   char go;
   for (go=-127;go<127;go++){
     drive(1,go);
@@ -34,21 +30,24 @@ void drive(char motor, char vel){
   char dir;
   if (vel >=0){
     dir = 0x88 + motor*4;
-  }else{
+  }
+  else{
     dir = 0x8a + motor*4;
+    vel ^= 0xFF;
   }
   usart2_tx(dir);
   usart2_tx(vel&0x7F);
 }
 
 void usart2_init(unsigned int baud){
-	UBRR2H = (unsigned char)(baud >> 8); //set baud rate register to correct baud
-	UBRR2L = (unsigned char)baud;
-	UCSR2B = (1<<RXEN2)|(1<<TXEN2); //enable tx and rx
-	UCSR2C = (1<<USBS2)|(3<<UCSZ20); //set data format (8 bit w/ 2 stops)
+  UBRR2H = (unsigned char)(baud >> 8); //set baud rate register to correct baud
+  UBRR2L = (unsigned char)baud;
+  UCSR2B = (1<<RXEN2)|(1<<TXEN2); //enable tx and rx
+  UCSR2C = (1<<USBS2)|(3<<UCSZ20); //set data format (8 bit w/ 2 stops)
 }
 
 void usart2_tx(unsigned char data){
-	while (!(UCSR2A & (1<<UDRE2))); //wait until all transmissions are done
-	UDR2 = data;  //shove the data into the buffer
+  while (!(UCSR2A & (1<<UDRE2))); //wait until all transmissions are done
+  UDR2 = data;  //shove the data into the buffer
 }
+
