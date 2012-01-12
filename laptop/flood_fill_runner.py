782e828b6f98fd1e1007296f0b12dee06e1e1106
Jacob Hurwitz <jhurwitz@mit.edu>
2012-01-12 12:10:20 -0500

    make flood fill run 100 trials

--- laptop/flood_fill_runner.py
+++ laptop/flood_fill_runner.py
@@ -3,10 +3,11 @@ import numpy as np
 import profile
 
 data = np.load("flood_fill_data.npy")
+data_list = [data.copy() for i in range(100)]
 
 def main():
-    d = data.copy()
-    flood_fill.flood_fill(d)
+    for d in data_list:
+        flood_fill.flood_fill(d)
 
 profile.run('main()')
 
