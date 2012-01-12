f710433309a6825db44ff45dec4585831cabc683
David Lawrence <dlaw@dlaw.me>
2012-01-12 16:03:27 -0500

    moved a bunch of files

--- laptop/visual_servo.py
+++ laptop/visual_servo.py
@@ -1,8 +1,12 @@
-import arduino
+import arduino, freenect, cv, frame_convert, numpy
+
+freenect.sync_get_video()[0]
+
+freenect.sync_get_depth()[0]
+
 
 accum_err = 0
 last = 0
-
 def visual_servo(theta, kp=.5, ki=0, kd=0):
     accum_err += theta
     p, i, d, last = theta, accum_err, theta - last, theta
