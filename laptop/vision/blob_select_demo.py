d7b772ebcdcc6c3a95caf738451306c4ea0009f0
Jacob Hurwitz <jhurwitz@mit.edu>
2012-01-12 20:55:08 -0500

    working blob_select_demo, added blob tracking to visual servo code

--- laptop/vision/blob_select_demo.py
+++ laptop/vision/blob_select_demo.py
@@ -6,7 +6,9 @@ import numpy as np
 
 current_hue = 0
 threshold = 10
-max_sq_dist_from_true = 45000
+max_sq_dist_from_true = 450000
+sat_const = 12
+val_const = 8
 min_area = 1000
 max_area = 35000
 color_fill = None
@@ -15,9 +17,8 @@ def change_hue(value):
     global current_hue, color_fill
     current_hue = value
     hsv_pixel = np.array([[[current_hue, 255, 255]]], dtype='uint8')
-    rgb_pixel = hsv_pixel.copy()
-    cv.CvtColor(cv.fromarray(hsv_pixel), cv.fromarray(rgb_pixel), cv.CV_HSV2RGB)
-    color_fill = cv.RGB(rgb_pixel[0,0,0], rgb_pixel[0,0,1], rgb_pixel[0,0,2])
+    color_fill = hsv_pixel.copy()
+    cv.CvtColor(cv.fromarray(hsv_pixel), cv.fromarray(color_fill), cv.CV_HSV2RGB)
 change_hue(0)
 
 def change_threshold(value):
@@ -27,6 +28,14 @@ def change_threshold(value):
 def change_max_dist(value):
     global max_sq_dist_from_true
     max_sq_dist_from_true = value
+
+def change_sat_const(value):
+    global sat_const
+    sat_const = value
+
+def change_val_const(value):
+    global val_const
+    val_const = value
 
 def change_min_area(value):
     global min_area
@@ -39,22 +48,29 @@ def change_max_area(value):
 def show_blobs():
     rgb = freenect.sync_get_video()[0]
     depth = freenect.sync_get_depth()[0].astype('float32')
-    blob_data = blob_select.blob_select(rgb, depth, target_hue = current_hue, hue_tolerance = threshold, sat_val_tolerance = max_sq_dist_from_true, min_blob_area = min_area, max_blob_area = max_area)
+    blob_data, filtered = blob_select.blob_select(rgb, depth, target_hue = current_hue, hue_tolerance = threshold, sat_val_tolerance = max_sq_dist_from_true, sat_c = sat_const, val_c = val_const, min_blob_area = min_area, max_blob_area = max_area)
+    colored = filtered[:,:,np.newaxis] * color_fill[...,::-1]
+    colored = colored.astype(np.uint8)
+    cv.ShowImage('Filtered', cv.fromarray(colored))
     draw_img = np.zeros((rgb.shape[0], rgb.shape[1], 3))
     for color, size, avg_r, avg_c, avg_d, var_d in blob_data:
-        cv.Circle(cv.fromarray(draw_img), (int(avg_c), int(avg_r)), int((1.0*size/np.pi)**0.5), color_fill)
+        cv.Circle(cv.fromarray(draw_img), (int(avg_c), int(avg_r)), int((1.0*size/np.pi)**0.5), cv.RGB(color_fill[0,0,0], color_fill[0,0,1], color_fill[0,0,2]))
     cv.ShowImage('Blobs', cv.fromarray(draw_img))
 
 def show_video():
     cv.ShowImage('Video', frame_convert.video_cv(freenect.sync_get_video()[0]))
 
-cv.NamedWindow('Blobs')
 cv.NamedWindow('Video')
-cv.CreateTrackbar('Target hue (0-180)', 'Blobs', current_hue, 180, change_hue)
-cv.CreateTrackbar('Hue tolerance (+/-)', 'Blobs', threshold, 90, change_threshold)
-cv.CreateTrackbar('Max squared distance from true color', 'Blobs', max_sq_dist_from_true, 100000, change_max_dist)
-cv.CreateTrackbar('Min blob area', 'Blobs', min_area, 3000, change_min_area)
-cv.CreateTrackbar('Max blob area', 'Blobs', max_area, 100000, change_max_area)
+cv.NamedWindow('Filtered')
+cv.NamedWindow('Blobs')
+cv.NamedWindow('Controls')
+cv.CreateTrackbar('Target hue (0-180)', 'Controls', current_hue, 180, change_hue)
+cv.CreateTrackbar('Hue tolerance (+/-)', 'Controls', threshold, 90, change_threshold)
+cv.CreateTrackbar('Max squared distance from true color', 'Controls', max_sq_dist_from_true, 1000000, change_max_dist)
+cv.CreateTrackbar('Saturation constant (x10)', 'Controls', sat_const, 30, change_sat_const)
+cv.CreateTrackbar('Value constant (x10)', 'Controls', val_const, 30, change_val_const)
+cv.CreateTrackbar('Min blob area', 'Controls', min_area, 3000, change_min_area)
+cv.CreateTrackbar('Max blob area', 'Controls', max_area, 100000, change_max_area)
 
 print('Press ESC in window to stop')
 
--- laptop/visual_servo.py
+++ laptop/visual_servo.py
@@ -1,13 +1,32 @@
-import arduino, freenect, cv, frame_convert, numpy
+#import arduino, freenect, cv, frame_convert, numpy
+import freenect, cv, frame_convert, numpy
+import vision.blob_select as blob_select
 
-freenect.sync_get_video()[0]
-
-freenect.sync_get_depth()[0]
-
+def track_ball():
+    global accum_err, last
+    rgb = freenect.sync_get_video()[0]
+    depth = freenect.sync_get_depth()[0].astype('float32')
+    blob_data, filtered = blob_select.blob_select(rgb, depth, target_hue = 0, hue_tolerance = 10, sat_val_tolerance = 45000, sat_c = 1.2, val_c = 0.8, min_blob_area = 1000, max_blob_area = 35000)
+    # select the blob with the largest size
+    # (there should be very few, so we can be slow)
+    biggest = None
+    biggest_idx = None
+    for idx, (colors, size, avg_r, avg_c, avg_d, var_d) in enumerate(blob_data):
+        if biggest is None or size > biggest:
+            biggest = size
+            biggest_idx = idx
+    if biggest is None: #move randomly
+        accum_err = 0
+        last = 0
+        turn = numpy.random.rand(1)[0]
+        arduino.set_motors(.3 + turn, -(.3 - turn))
+    else:
+        visual_servo(320-avg_c)
 
 accum_err = 0
 last = 0
 def visual_servo(theta, kp=.5, ki=0, kd=0):
+    global accum_err, last
     accum_err += theta
     p, i, d, last = theta, accum_err, theta - last, theta
     turn = kp*p + ki*i + kd*d
