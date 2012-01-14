e1b05c286640a081a985994c03ef7074d5056a97
David Honig <pleit@maslab-team-7.(none)>
2012-01-13 19:42:20 -0500

    Fixed contacts

--- laptop/visual_servo.py
+++ laptop/visual_servo.py
@@ -8,7 +8,7 @@ import time
 accum_err = 0
 last = 0
 speed = 0.25
-def visual_servo(theta, kp=-.001, ki=0, kd=0):
+def visual_servo(theta, kp=-.003, ki=0, kd=0):
     global accum_err, last
     accum_err += theta
     p, i, d, last = theta, accum_err, theta - last, theta
@@ -21,12 +21,12 @@ def visual_servo(theta, kp=-.001, ki=0, kd=0):
 
 while True:
     raw_image = freenect.sync_get_video()[0]
-    image = numpy.empty((240,320,3), 'uint8')
+    image = np.empty((240,320,3), 'uint8')
     cv.Resize(cv.fromarray(raw_image), cv.fromarray(image))
     cv.CvtColor(cv.fromarray(image), cv.fromarray(image), cv.CV_RGB2HSV)
     depth = freenect.sync_get_depth()[0].astype('float32')
-    good = color.select(image, [175,255,255], [30,150,250]).astype('uint32')
-    blob_data = blobs.find_blobs(good, depth, 1000, 30000)
+    good = color.select(image, [175,255,255], [15,150,250]).astype('uint32')
+    blob_data = blobs.find_blobs(good, depth, 100, 30000)
 
     # select the blob with the largest size
     # (there should be very few, so we can be slow)
