e4c2c5e774fb5772fd17b5245307cf6de044649c
David Lawrence <dlaw@dlaw.me>
2012-01-14 19:22:31 -0500

    Convert to HSV

--- laptop/vision/kinect.py
+++ laptop/vision/kinect.py
@@ -11,12 +11,19 @@ freenect.start_depth(dev) # sync_get_depth hangs if we don't do this
 freenect.close_device(dev) # close the device so that c_sync can open it
 
 def get_images():
+    """
+    Returns the tuple (timestamp, video, depth) from the Kinect.
+    timestamp is in seconds relative to an arbitrary zero time.
+    video is a (320,240,3)-array of uint8s in HSV format.
+    depth is a (320,240)-array of uint16s.
+    """
     raw_video, timestamp = freenect.sync_get_video()
     raw_depth = freenect.sync_get_depth()[0]
     cv.Resize(cv.fromarray(raw_video), cv.fromarray(video))
+    cv.CvtColor(cv.fromarray(video), cv.fromarray(video), cv.CV_RGB2HSV)
     # Must use nearest-neighbor for depth because of 2047 values
     cv.Resize(cv.fromarray(raw_depth), cv.fromarray(depth), cv.CV_INTER_NN)
-    return timestamp, video, depth
+    return timestamp / 60008625., video, depth # convert to seconds
 
 get_images()
 print("done")
