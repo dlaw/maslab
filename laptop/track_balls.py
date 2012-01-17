eba358deff3845464d17d90473ccc5b482371e52
Jacob Hurwitz <jhurwitz@mit.edu>
2012-01-17 15:28:16 -0500

    fix track_balls

--- laptop/track_balls.py
+++ laptop/track_balls.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python2.7
 
-import arduino, freenect, cv, numpy as np
+import arduino, freenect, cv, numpy as np, time
 from vision import color, blobs, kinect, walls
 
-const = {'target_hue': 175,
-         'hue_c': 15,
-         'sat_c': 150,
-         'val_c': 200,
-         'min_area': 100, #might need to bump this up to avoid false positives
-         'wall_target_hue': 114,
-         'wall_hue_c': 15,
-         'wall_sat_c': 150,
-         'wall_val_c': 200,
-         'kinect_fov': np.pi/2, #fix this constant
-         'depth_scaler': 0.001, #fix this constant
-         'small_angle': 0.1, #fix this constant
-         'after_losing_sight': 30} #fix this constant
+color_defs = [('red', 175, 1./15, 1./150, 1./250),
+              ('yellow', 23, 1./10, 1./100, 1./140),
+              ('green', 50, 1./15, 1./250, 1./350),
+              ('blue', 114, 1./15, 1./400, 1./400)]
 
-colors = np.array([[const['target_hue'], 1./const['hue_c'],
-                    255, 1./const['sat_c'],
-                    255, 1./const['val_c']],
-                   [const['wall_target_hue'], 1./const['wall_hue_c'],
-                    255,  1./const['wall_sat_c'],
-                    255, 1./const['wall_val_c']]], 'float64')
+constants = np.vstack([[hue, 255., 255., hue_c, sat_c, val_c]
+                       for name, hue, hue_c, sat_c, val_c in color_defs])
+
+const = {'min_area': 100,
+         'kinect_fov': np.pi/2,
+         'depth_scaler': 0.001,
+         'small_angle': 0.1,
+         'after_losing_sight': 30}
 
 cycles_since_lost_sight = 0
+start_time = time.time()
+
 def move():
     global just_lost_sight
     t, image, depth = kinect.get_images()
-    result = color.identify(image, colors)
-    top, bottom, c = walls.identify(result, 1, [])
-    cv.CvtColor(cv.fromarray(image), cv.fromarray(image), cv.CV_HSV2BGR)
-    image /= 2
-    blob_data = blobs.find_blobs(result, depth, const['min_area'])
+    colors = color.identify(image, constants)
+    top, bottom, wallcolor = walls.identify(colors, 3, 2, 2)
+    blob_data = blobs.find_blobs(colors, depth, const['min_area'])
     if not len(blob_data): #no blobs found
         cycles_since_lost_sight += 1
         if cycles_since_lost_sight < const['after_losing_sight']: #drive straight
@@ -43,13 +36,14 @@ def move():
         cycles_since_lost_sight = 0
         blob_to_track = max(blob_data, key = lambda blob: abs(160-blob['col'][0]))
         angle = const['kinect_fov'] / 320. * (160 - blob_to_track['col'][0])
-        if abs(angle) < small_angle: #move towards it
+        if abs(angle) < const['small_angle']: #move towards it
             dist = const['depth_scaler'] * blob_to_track['depth'][0]
             arduino.drive(dist, angle)
         else: #rotate only (don't drive)
             arduino.rotate(angle)
 
 if __name__ == '__main__':
-    while True:
+    while time.time() < start_time + 179:
         move()
+    arduino.set_motors(0, 0)
 
