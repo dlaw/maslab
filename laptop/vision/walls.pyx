324c231f3fff509f115f91aa5f16c28be9fc5cfa
David Lawrence <dlaw@dlaw.me>
2012-01-17 01:47:51 -0500

    Allow skipping pixels

--- laptop/vision/walls.pyx
+++ laptop/vision/walls.pyx
@@ -2,31 +2,33 @@ import numpy as np
 cimport cython, numpy as np
 
 def identify(np.ndarray[np.int32_t, ndim=2] img,
-             top_color, wall_colors, min_height = 4):
+             top_color, wall_colors, min_height = 4, max_skip = 2):
     cdef np.ndarray[np.int32_t] top = np.empty(img.shape[1], np.int32)
     cdef np.ndarray[np.int32_t] bottom = np.empty(img.shape[1], np.int32)
     cdef np.ndarray[np.int32_t] color = np.empty(img.shape[1], np.int32)
-    cdef int count, mark, col, row
+    cdef int good_count, skip_count, start_mark, end_mark, col, row
     for col in range(img.shape[1]):
-        count = 0
+        good_count = 0
         for row in range(img.shape[0] - 1, -1, -1): # scan up to top of blue
             if img[row, col] == top_color:
-                if count == 0:
-                    mark = row
-                count += 1
-            elif count < min_height:
-                count = 0
+                skip_count = 0
+                if good_count == 0:
+                    start_mark = row
+                good_count += 1
             else:
-                top[col] = row + 1
-                break
-        else:
-            top[col] = -1
-            bottom[col] = -1
-            color[col] = -1
+                skip_count += 1
+                if skip_count == max_skip:
+                    if good_count < min_height:
+                        good_count = 0
+                    else:
+                        top[col] = row + 1
+                        break
+        else: # can't ID wall in this column
+            top[col] = bottom[col] = color[col] = -1
             continue
         bottom[col] = img.shape[0] - 1
         color[col] = -1
-        for row in range(mark, img.shape[0]): # scan down to bottom of wall
+        for row in range(start_mark, img.shape[0]): # scan down to bottom of wall
             if img[row, col] in wall_colors:
                 color[col] = img[row, col]
             else:
