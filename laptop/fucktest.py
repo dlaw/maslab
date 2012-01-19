2b2f8e47f5d505439d6f5fec9a874f0b193ee249
David Lawrence <dlaw@dlaw.me>
2012-01-19 14:36:03 -0500

    make fucktest pythonic

--- laptop/fucktest.py
+++ laptop/fucktest.py
@@ -2,20 +2,16 @@ import arduino
 import time
 
 time.sleep(2)
-timer=0
-if (not arduino.is_alive()):
-	print "arduino is dead"
-else:	
-	while (timer < 150):
-		arduino.set_motors(1,-1)
-		time.sleep(1)
-		arduino.set_motors(-1,1)
-		time.sleep(1)
-		timer+=1
-	timer = 0
-	while (timer < 150):
-		arduino.set_motors(1,1)
-		time.sleep(1)
-		arduino.set_motors(1,1)
-		time.sleep(1)
-		timer+=1
+assert arduino.is_alive()
+
+for timer in range(150):
+    arduino.set_motors(1,-1)
+    time.sleep(1)
+    arduino.set_motors(-1,1)
+    time.sleep(1)
+
+for timer in range(150):
+    arduino.set_motors(1,1)
+    time.sleep(1)
+    arduino.set_motors(-1,-1)
+    time.sleep(1)
