4945598a2d958487148289b817de0c348ecb61b5
josh gordonson <muffin@mit.edu>
2012-01-19 16:56:55 -0500

    longer fucktest

--- laptop/fucktest.py
+++ laptop/fucktest.py
@@ -4,13 +4,13 @@ tsleep = .5
 time.sleep(2)
 assert arduino.is_alive()
 print "arduino is alive"
-for timer in range(150):
+for timer in range(600):
     arduino.set_motors(1,-1)
     time.sleep(tsleep)
     arduino.set_motors(-1,1)
     time.sleep(tsleep)
 
-for timer in range(150):
+for timer in range(600):
     arduino.set_motors(1,1)
     time.sleep(tsleep)
     arduino.set_motors(-1,-1)
