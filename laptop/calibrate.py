1548ca30a191c72ea6459442a03bd947d19fae64
David Lawrence <dlaw@dlaw.me>
2012-01-19 15:49:01 -0500

    update calibrate for yellow wall boxing

--- laptop/calibrate.py
+++ laptop/calibrate.py
@@ -13,6 +13,9 @@ cv.NamedWindow('Video', cv.CV_WINDOW_NORMAL)
 cv.NamedWindow('Depth', cv.CV_WINDOW_NORMAL)
 cv.NamedWindow('Constants')
 
+def width(variance):
+    return np.sqrt(1 + 12 * variance)
+
 def trackbar(index, color, prop, maxval, invert=False, window='Constants'):
     def update(value):
         constants[index] = 1./value if invert else value
@@ -35,25 +38,18 @@ def show_video():
     yellow_walls = blobs.find_blobs(colors, depth, color=1, min_size=10)
     green_walls = blobs.find_blobs(colors, depth, color=2, min_size=10)
     color.colorize(image, constants, colors)
-    """
-    temporary -- fix this up later!
-    """
-    green_data = blobs.find_blobs(colors, depth, color=2)
-    for g in green_data:
-        #formula: length = sqrt( 1 + 12 * variance )
-        w = np.sqrt(1+12*g['col'][1])
-        h = np.sqrt(1+12*g['row'][1])
-        x = g['col'][0]
-        y = g['row'][0]
-        ul = (int(x-w/2), int(y-h/2))
-        ur = (int(x+w/2), int(y-h/2))
-        lr = (int(x+w/2), int(y+h/2))
-        ll = (int(x-w/2), int(y+h/2))
-        cv.FillPoly(cv.fromarray(image), [[ul, ur, lr, ll]], [45,200,200])
-            #make the rectangle a different shade of green so we can see it in the output
-    """
-    end temporary section
-    """
+    for ball in balls:
+        cv.Circle(cv.fromarray(image),
+                  (int(ball['row'][0] + .5), int(ball['col'][0] + .5)),
+                  int(1.5 + np.sqrt(ball['area'] / 3.14)),
+                  (0, 0, 255))
+    for wall in yellow_walls:
+        cx, wx = ball['row'][0], width(ball['row'][1])/2
+        cy, wy = ball['col'][0], width(ball['col'][1])/2
+        cv.Rectangle(cv.fromarray(image),
+                     (int(cx - wx + .5), int(cy - wy + .5)),
+                     (int(cx + wx + .5), int(cy + wy + .5)),
+                     (0, 0, 255))
     cv.CvtColor(cv.fromarray(image), cv.fromarray(image), cv.CV_HSV2BGR)
     cv.ShowImage('Video', cv.fromarray(image))
     cv.ShowImage('Depth', cv.fromarray(depth << 5))
