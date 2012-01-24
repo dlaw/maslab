384cf5f785541557d09739c779e4d1c87be950d7
Jacob Hurwitz <jhurwitz@mit.edu>
2012-01-24 13:30:14 -0500

    GoToBall, GoToYellow

--- laptop/constants.py
+++ laptop/constants.py
@@ -1,5 +1,10 @@
 dump_time = 30 # seconds before end-of-match to start looking for yellow wall
 
+drive_speed = .8 # standard drive forward speed
+ball_follow_kp = .004
+close_ball_row = 80 # snarf the ball if its center is below this row
+wall_follow_kp = .004
+
 # Ball snarfing: when we consume a ball
 snarf_time = .7 # how long to snarf a ball after losing sight of it
 snarf_speed = .8 # how fast to drive while snarfing
