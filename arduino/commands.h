71606c9926cecf9cd89e299d75a994980aaea821
David Lawrence <dlaw@dlaw.me>
2012-01-25 15:54:41 -0500

    modify arduino code for open-loop with stock motors.
    this marks a permanent divergence from branch master.

--- arduino/commands.h
+++ arduino/commands.h
@@ -1,26 +1,4 @@
 #include "qik.h"
-#include "nav.h"
-#include "external_interrupt.h"
-
-inline void SEND_INT32(uint32_t val){
-  usart0_tx((unsigned char) (val >> 24) & 0xFF);
-  usart0_tx((unsigned char) (val >> 16) & 0xFF);
-  usart0_tx((unsigned char) (val >> 8) & 0xFF);
-  usart0_tx((unsigned char) val & 0xFF);
-}
-inline void SEND_INT16(uint16_t val){
-  usart0_tx((unsigned char) (val >> 8) & 0xFF);
-  usart0_tx((unsigned char) val & 0xFF);
-}
-#define TO_INT32(arr,i) (arr[i] + ((uint32_t) arr[i+1]) << 8 + \
-    ((uint32_t) arr[i+2]) << 16 + ((uint32_t) arr[i+3]) << 24)
-#define TO_INT16(arr,i) (arr[i] + ((uint16_t) arr[i+1] << 8))
-
-int32_t target_ltime;
-int32_t target_rtime;
-
-volatile int dl;
-volatile int dr;
 
 // serdata is an array of volatile unsigned chars
 typedef volatile unsigned char serdata[];
@@ -28,95 +6,35 @@ typedef volatile unsigned char serdata[];
 // responder is the type of a function that takes serdata and returns void
 typedef void (*responder) (serdata);
 
-long timeout;
-
 // command 0x00
 // Send an ack
 void ack(serdata data){
   usart0_tx(0x00);
 }
 
-// commnad 0x01
+// command 0x01
 // Set motor speeds and send an ack
-void setmotors(serdata data){
-  drive(data[1],data[0]);
+void set_drive(serdata data){
+  target_lvel = data[1];
+  target_rvel = data[0];
   usart0_tx(0x00);
-  navstate = 0; // override whatever other control loop is happening
-  timeout = 0;
 }
 
 // command 0x02
-//Send an IR sensor reading
-void sendir(serdata data){
+// Send analog data
+void send_analog(serdata data){
   usart0_tx(analog[adcmap[data[0]]]);
 }
 
-// command 0x07
-void changeparam(serdata data) {
-	unsigned char param = data[0];
-	parameters[param] = TO_INT16(data,1);
-	usart0_tx(param);
-}
-
-// command 0x08
-void sendticks(serdata data) {
-  SEND_INT16(tickl);
-  SEND_INT16(tickr);
-}  
-
-void setmotorspeed(serdata data) {
-  // DO NOT CHANGE THESE
-  int32_t new_ltime = (uint32_t) data[4] + ((uint32_t) data[5] << 8) + ((uint32_t) data[6] << 16) + ((uint32_t) data[7] << 24);
-  int32_t new_rtime = (uint32_t) data[0] + ((uint32_t) data[1] << 8) + ((uint32_t) data[2] << 16) + ((uint32_t) data[3] << 24);
-  navstate = 2;
-  
-  if ((new_ltime < 0 & target_ltime > 0) | (new_ltime > 0 & target_ltime < 0)) {
-    dl = (new_ltime > 0) ? 48 : -48;
-  }
-  if ((new_rtime < 0 & target_rtime > 0) | (new_rtime > 0 & target_rtime < 0)) {
-    dr = (new_rtime > 0) ? 48 : -48;
-  }
-  
-  target_ltime = new_ltime;
-  target_rtime = new_rtime;
-  
-  drive(dl, dr);
-  
-  usart0_tx(0x00);
-}
-
-void buttonpressed(serdata data) {
+// command 0x03
+// get state of start switch
+void button_pressed(serdata data) {
   usart0_tx(digitalRead(53));
 }
 
-void setsucker(serdata data) {
-  if (data[0] == 0x00) {
-    digitalWrite(6, LOW);
-  } else {
-    digitalWrite(6, HIGH);
-  }
-  usart0_tx(0x00);
-}
-
-void sethelix(serdata data) {
-  if (data[0] == 0x00) {
-    digitalWrite(7, LOW);
-  } else {
-    digitalWrite(7, HIGH);
-  }
-  usart0_tx(0x00);
-}
-
-void setdoor(serdata data) {
-  if (data[0] == 0x00) {
-    OCR1A=130;
-  } else {
-    OCR1A=340;
-  }
-  usart0_tx(0x00);
-}
-
-void getbumpsensors(serdata data) {
+// command 0x04
+// get state of bump sensors
+void get_bump(serdata data) {
   char out = digitalRead(52) << 5;
   out += digitalRead(51) << 4;
   out += digitalRead(50) << 3;
@@ -126,27 +44,29 @@ void getbumpsensors(serdata data) {
   usart0_tx(out);
 }
 
+// command 0x05
+// set aux motor
+void set_motor(serdata data) {
+  // data[1] is index of motor: 0 helix, 1 sucker, 2 door
+  // data[0] is new value
+  if (data[1] == 2)
+    OCR1A = data[0] ? 340 : 130;
+  else
+    digitalWrite(data[1] ? 6 : 7, data[0] ? HIGH : LOW);
+  usart0_tx(0x00);
+}
+
 // How many bytes of data will follow each command?
-unsigned char commands[16]={
-  0,2,1,5,8,0,0,3,0,0,8,0,1,1,1,0
+unsigned char commands[] = {
+  0, 2, 1, 0, 0, 2
 };
 
 // What function shall be called to respond to each command?
-responder responses[15]={
+responder responses[] = {
   &ack,
-  &setmotors,
-  &sendir,
-  &ack, // depricated
-  &ack, // depricated
-  &ack, // depricated
-  &ack, // depricated
-  &changeparam,
-  &sendticks,
-  &ack, // depricated
-  &setmotorspeed,
-  &buttonpressed,
-  &setsucker,
-  &sethelix,
-  &setdoor,
-  &getbumpsensors
+  &set_drive,
+  &send_analog,
+  &button_pressed,
+  &get_bump,
+  &set_motor
 };
--- arduino/external_interrupt.h
+++
@@ -1,9 +0,0 @@
-#include <avr/interrupt.h>
-
-volatile int tickl=0;            //left motor encoder tick counter
-volatile int tickr=0;            //right motor encoder tick counter
-
-void ext_int_init(void){		//hardcode the pins that you want!
-  EICRB |= (1<<ISC40)|(1<<ISC50);   //configure any logical change interrupt for int4 and int5
-  EIMSK |= (1<<INT4)|(1<<INT5);     //enable int4 and int5
-}
