67c619902f3db228010a87a2c5037257a6edada5
David Honig <pleit@mit.edu>
2012-01-27 11:26:51 -0500

    tester for FollowWall

--- laptop/wall_follow_test.py
+++ laptop/wall_follow_test.py
@@ -6,10 +6,14 @@ time.sleep(1) # wait for arduino and kinect to power up
 
 want_change = True
 
+class FollowWallTest(navigation.FollowWall):
+    def next(self, time_left):
+        return self.default_action()
+
 def run():
     global want_change
-    print("starting state_tester.py")
-    state = navigation.LookAround()
+    print("starting wall_follow_test.py")
+    state = FollowWallTest(on_left=False)
     arduino.set_helix(True)
     arduino.set_sucker(True)
     fake_time_left = 180
@@ -24,22 +28,8 @@ def run():
                 arduino.set_helix(False)
                 exit()
             s = s.split(" ")
-            if len(s) > 1:
-                fake_time_left = int(s[1])
-            else:
-                fake_time_left = 180
-            new_state = None
-            for class_name in ["navigation", "maneuvering"]:
-                try:
-                    new_state = eval(class_name + "." + s[0])
-                    break
-                except AttributeError:
-                    continue
-            if new_state is None:
-                print("{0} was not found in any of the classes".format(s[0]))
-            else:
-                state = new_state
-                print("State manually changed to {0}".format(state))
+            constants.wall_follow_kp = float(s[0])
+            constants.wall_follow_kd = float(s[1])
         kinect.process_frame()
         try:
             new_state = state.next(fake_time_left)
@@ -49,6 +39,9 @@ def run():
         except Exception, ex:
             print("{0} while attempting to change states".format(ex))
             traceback.print_exc(file=sys.stdout)
+        if not isinstance(state, FollowWallTest):
+            print("Back to FollowWallTest(on_left=False)")
+            state = FollowWallTest(on_left=False)
 
 def change_state(*args):
     global want_change
