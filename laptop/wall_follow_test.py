54ba102b8ca9ef808df2e3261318151480ad552f
David Honig <pleit@mit.edu>
2012-01-30 16:47:40 -0500

    fix wall_follow_test

--- laptop/wall_follow_test.py
+++ laptop/wall_follow_test.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python2.7
 
-import signal, time, arduino, kinect, navigation, maneuvering, sys, traceback, constants, main, random
+import signal, time, arduino, kinect, navigation, maneuvering, sys, traceback, constants, main, random, numpy as np
 
 time.sleep(1) # wait for arduino and kinect to power up
 
 want_change = True
 
-class FollowWall(main.State): # PDD controller
+class FollowWallTest(main.State): # PDD controller
     timeout = random.uniform(.5, 1) * constants.follow_wall_timeout
     def __init__(self):
+        """
         if np.random.rand() < constants.prob_change_wall_follow_dir:
             constants.wall_follow_on_left = not constants.wall_follow_on_left
+        """
+        constants.wall_follow_on_left = True
         self.ir, self.dir = (0, -1) if constants.wall_follow_on_left else (3, 1)
         self.time_wall_seen = time.time()
         self.turning_away = False
         self.last_p, self.last_d = None, None
+    """
     def on_stuck(self):
         if any(arduino.get_bump()):
             return maneuvering.Unstick()
         # TODO: check if IRs are totally unreasonable.
+        return self.default_action()
+    """
+    def next(self, time_left):
         return self.default_action()
     def default_action(self):
         side_ir = arduino.get_ir()[self.ir]
@@ -27,7 +34,7 @@ class FollowWall(main.State): # PDD controller
         dd = (d - self.last_d) if self.last_d else 0
         self.last_p, self.last_d = p, d
         if time.time() - self.time_wall_seen > constants.lost_wall_timeout:
-            return LookAround()
+            return navigation.LookAround()
         elif (max(arduino.get_ir()[1:-1]) > constants.wall_follow_dist
               or self.turning_away): # too close in front
             self.turning_away = True
@@ -35,6 +42,7 @@ class FollowWall(main.State): # PDD controller
             drive = 0
             turn = constants.wall_follow_turn * -1 * self.dir
             arduino.drive(drive, turn)
+            print("A {d:4.2f} {t:4.2f}".format(d=drive, t=turn))
             if (max(arduino.get_ir()[1:-1]) < constants.wall_follow_dist and
                 side_ir > constants.wall_follow_limit):
                 self.turning_away = False
@@ -47,10 +55,12 @@ class FollowWall(main.State): # PDD controller
                                        -constants.wall_follow_max_turn,
                                        constants.wall_follow_max_turn)
             arduino.drive(drive, turn)
+            print("B {d:4.2f} {t:4.2f}".format(d=drive, t=turn))
         else: # lost wall but not timed out, so turn into the wall
             drive = constants.wall_follow_drive / 2
             turn = constants.wall_follow_turn
             arduino.drive(drive, turn)
+            print("C {d:4.2f} {t:4.2f}".format(d=drive, t=turn))
 
 def run():
     global want_change
