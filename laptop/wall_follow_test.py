99f479df2b44bee8e411cdc8f42b40fff92aec7a
David Honig <pleit@mit.edu>
2012-01-30 15:10:40 -0500

    update wall_follow_test for PDD

--- laptop/wall_follow_test.py
+++ laptop/wall_follow_test.py
@@ -1,50 +1,43 @@
 #!/usr/bin/python2.7
 
-import signal, time, arduino, kinect, navigation, maneuvering, sys, traceback, constants, main
+import signal, time, arduino, kinect, navigation, maneuvering, sys, traceback, constants, main, random
 
 time.sleep(1) # wait for arduino and kinect to power up
 
 want_change = True
 
-class FollowWallTest(main.State):
-    timeout = constants.follow_wall_timeout # times out to LookAround
+class FollowWallTest(main.State): # PDD controller
+    timeout = random.uniform(.5, 1) * constants.follow_wall_timeout
     def __init__(self):
-        self.on_left = True
-        self.ir = 0 if self.on_left else 3
-        self.dir = -1 if self.on_left else 1 # sign of direction to turn into wall
+        constants.wall_follow_on_left = True
+        self.ir, self.dir = (0, -1) if constants.wall_follow_on_left else (3, 1)
         self.time_wall_seen = time.time()
-        self.turning_in = False
-        self.err = None
+        self.turning_away = False
+        self.last_p, self.last_d = None, None
     def next(self, time_left):
         return self.default_action()
     def default_action(self):
-        dist = arduino.get_ir()[self.ir]
-        front = max(arduino.get_ir()[1:-1])
-        self.last_err, self.err = self.err, constants.wall_follow_dist - dist
-        if self.last_err is None: self.last_err = self.err # initialize D to 0
-        if self.turning_in or front > constants.wall_follow_dist: # too close in front
+        side_ir = arduino.get_ir()[self.ir]
+        p = constants.wall_follow_dist - side_ir
+        d = (err - self.last_p) if self.last_p else 0
+        dd = (d - self.last_d) if self.last_d else 0
+        self.last_p, self.last_d = p, d
+        if self.turning_away or max(arduino.get_ir()[1:-1]) > constants.wall_follow_dist: # too close in front
+            self.turning_away = True
             self.time_wall_seen = time.time()
-            self.turning_in = True
-            drive = 0
-            turn = constants.wall_follow_turn * -1 * self.dir
-            print("A {0:1.4} {3:1.4} drive {1:1.4} {2:1.4}".format(dist, float(drive), float(turn), front))
-            arduino.drive(drive, turn)
+            arduino.drive(0, constants.wall_follow_turn * -1 * self.dir)
             if max(arduino.get_ir()[1:-1]) < constants.wall_follow_dist and dist > constants.wall_follow_limit:
-                self.turning_in = False
+                self.turning_away = False
         elif dist > constants.wall_follow_limit: # if we see a wall
             self.time_wall_seen = time.time()
-            drive = constants.drive_speed
-            turn = self.dir * (constants.wall_follow_kp * self.err + constants.wall_follow_kd * (self.err - self.last_err))
-            print("B {0:1.4} {3:1.4} drive {1:1.4} {2:1.4}".format(dist, float(drive), float(turn), front))
-            arduino.drive(drive, turn)
+            arduino.drive(constants.drive_speed, self.dir * 
+                          (constants.wall_follow_kp * p +
+                           constants.wall_follow_kd * d +
+                           constants.wall_follow_kd * dd))
         elif time.time() - self.time_wall_seen < constants.lost_wall_timeout:
-            drive = constants.wall_follow_drive
-            turn = constants.wall_follow_turn * self.dir
-            print("C {0:1.4} {3:1.4} drive {1:1.4} {2:1.4}".format(dist, float(drive), float(turn), front))
-            arduino.drive(drive, turn)
+            arduino.drive(constants.wall_follow_drive, constants.wall_follow_turn * self.dir)
         else: # lost wall
-            print("D {0:1.4} {1:1.4}".format(dist, front))
-            return navigation.LookAround()
+            return LookAround()
 
 def run():
     global want_change
@@ -55,13 +48,14 @@ def run():
         if want_change:
             want_change = False
             arduino.drive(0, 0)
-            print "Enter kp and kd, separated by a space"
+            print "Enter kp, kd, and kdd, separated by spaces"
             s = raw_input("> ")
             if s == "":
                 exit()
             s = s.split(" ")
             constants.wall_follow_kp = float(s[0])
             constants.wall_follow_kd = float(s[1])
+            constants.wall_follow_kdd = float(s[2])
         kinect.process_frame()
         try:
             new_state = state.next(fake_time_left)
