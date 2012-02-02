8f8a97c07e65a8ea58735b73eaa06cd357551787
Jacob Hurwitz <jhurwitz@mit.edu>
2012-02-02 03:50:21 -0500

    forgot to add file

---
+++ laptop/variables.py
@@ -0,0 +1,7 @@
+import time
+
+number_possessed_balls = 0 # how many balls we currently possess in our "extra cheese" (third) level
+ball_attempts = 0 # how many times we've tried to get a ball since a new ball has entered the third level
+stalking_yellow = False # when we're stalking yellow, we can't follow walls
+time_last_seen_yellow = time.time()
+
